47b4a64cc6e10a1a0623df5f3a638b19372557b9
ga27pol <peng.chen@tum.de>
2018-07-05 21:39:56 +0200

    + upload parameter.py + change it back to calculate output of neuron all at once (I'm idiot) + keep forward_t, but only input layer works + for forward, small weight, roughly between [-1,1] + for forward_t, large weight - when some weight initlized with negative value, the membrane potential could be negative, not sure if it's a problem

--- Neuron.py
+++ Neuron.py
@@ -1,13 +1,14 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from layers import *
+import parameters as p
 import pdb
 
 class LIF_Neuron(object):
 
 	#hidden neuron and special neuron
 
-	def __init__(self, dim, vth = 30, tau_ref = 4, tau_m = 10, t_rest = 5, gmax = 30, tau_s = 5):
+	def __init__(self, dim, T, dt, vth = 30, tau_ref = 4, tau_m = 10, t_rest = 5, gmax = 30, tau_s = 5):
 		self.vth = vth
 		self.tau_ref = tau_ref # refractory time period
 		self.tau_m = tau_m
@@ -20,9 +21,11 @@ class LIF_Neuron(object):
 		self.PSP = np.zeros(self.dim)
 		self.I = np.zeros(self.dim)
 		self.v = np.zeros(self.dim)#membrane potential
+		self.T = T
+		self.dt = dt
 
-	def forward(self, x, T, dt, w):
-		time = np.arange(0, T + dt, dt) # total simulation time
+	def forward(self, x, w):
+		time = np.arange(0, self.T + self.dt, self.dt) # total simulation time
 		self.vm = np.zeros((self.dim, len(time))) # membrane potential
 		alpha_t = self.gmax * time / self.tau_s * np.exp(1 - time / self.tau_s)
 		for j in range(self.dim):
@@ -40,9 +43,10 @@ class LIF_Neuron(object):
 						t_rest = t + self.tau_ref
 		return self.vm, self.vm > self.vth
 
-	def forward_t(self, x, t, spike):
+	def forward_t(self, t, spike, w):
 		'''
 		spike: input spike list, if i-th neuron fires spike[i]=1, otherwise 0
+		w: weights of connection before this layer [dim_former_layer x dim_current_layer]
 		'''
 		dPSPdt = np.zeros(self.dim)
 		dIdt = np.zeros(self.dim)
@@ -52,148 +56,148 @@ class LIF_Neuron(object):
 				self.refract[i] -= 1
 			self.spike[i] = 0
 			dIdt[i] = -self.I[i]/self.tau_s
+			for j, s in enumerate(spike):
+				dIdt[i] += w[j][i] * s
 			dPSPdt[i] = (self.I[i] - self.PSP[i]) / self.tau_s
 			dvdt[i] = (self.PSP[i] - self.v[i]) / self.tau_m
 			#update PSP and I
 			self.I[i] += dIdt[i]
 			self.PSP[i] += dPSPdt[i]
-			if self.refract == 0:
+			if self.refract[i] == 0:
 				self.v[i] += dvdt[i]
 				if self.v[i] > self.vth:
 					self.v[i] = 0
 					self.spike[i] = 1
 					self.refract[i] = self.tau_ref
+			print(self.v[i])
+		return self.spike
+
 
 class hidden_neuron(LIF_Neuron):
 	def __init__(self, dim):
-		LIF_Neuron.__init__(self, dim, vth = p.vth_h, tau_ref = p.tau_ref_h, tau_m = p.tau_m_h, t_rest = p.t_rest_h, gmax = p.gmax_h, tau_s = p.tau_s_h)
+		LIF_Neuron.__init__(self, dim, p.T, p.dt, vth = p.vth_h, tau_ref = p.tau_ref_h, tau_m = p.tau_m_h, t_rest = p.t_rest_h, gmax = p.gmax_h, tau_s = p.tau_s_h)
 
 class output_neuron(LIF_Neuron):
-	def __init__(self, dim, T, dt, alpha = 5, beta = 0.05, gamma = 0):
-		LIF_Neuron.__init__(self, dim, vth = p.vth_o, tau_ref = p.tau_ref_o, tau_m = p.tau_m_o, t_rest = p.t_rest_o, gmax = p.gmax_o, tau_s = p.tau_s_o)
-		self.spikeT = np.zeros((dim, int(T/dt)))
-		self.T = T
-		self.dt = dt
+	def __init__(self, dim, alpha = 5, beta = 0.05, gamma = 0):
+		LIF_Neuron.__init__(self, dim, p.T, p.dt, vth = p.vth_o, tau_ref = p.tau_ref_o, tau_m = p.tau_m_o, t_rest = p.t_rest_o, gmax = p.gmax_o, tau_s = p.tau_s_o)
 		self.alpha = alpha
 		self.beta = beta
 		self.gamma = gamma
+		self.spikeT = np.zeros((dim, int(p.T/p.dt)))
 
-	def forward(self, x, t):
-		LIF_Neuron.forward_t(self, x, t)
-		self.spikeT[i, t] = self.spike
 
-	def decode(self, x):
+	# def forward(self, x, t):
+	# 	LIF_Neuron.forward_t(self, x, t)
+	# 	self.spikeT[i, t] = self.spike
+	# 	return self.spikeT
+
+	def decode(self, x, w):
 		time = np.arange(0, self.T + self.dt, self.dt)
-		_, out = self.forward(x, self.T, self.dt)
+		_, out = self.forward(x, w)
 		y_decode = np.zeros((self.dim, len(time)))
 		for n in range(self.dim):
 			for tf in np.where(out[n] > 0)[0]:
 				mask = np.zeros(len(time))
 				mask[tf :] = 1
-				y_decode[n] += self.alpha * (T - time[tf]) / T * np.exp(self.beta *(time[tf] - time)) * mask
+				y_decode[n] += self.alpha * (self.T - time[tf]) / self.T * np.exp(self.beta *(time[tf] - time)) * mask
 			y_decode[n] -= self.gamma
 		return out, y_decode
+	# def decode(self, x):
+	# 	time = np.arange(0, self.T + self.dt, self.dt)
+	# 	_, out = self.forward(x, self.T, self.dt)
+	# 	y_decode = np.zeros((self.dim, len(time)))
+	# 	for n in range(self.dim):
+	# 		for tf in np.where(out[n] > 0)[0]:
+	# 			mask = np.zeros(len(time))
+	# 			mask[tf :] = 1
+	# 			y_decode[n] += self.alpha * (T - time[tf]) / T * np.exp(self.beta *(time[tf] - time)) * mask
+	# 		y_decode[n] -= self.gamma
+	# 	return out, y_decode
 
 class special_neuron(LIF_Neuron):
 	def __init__(self):
 		LIF_Neuron.__init__(self, 1, vth = p.vth_o, tau_ref = p.tau_ref_o, tau_m = p.tau_m_o, t_rest = p.t_rest_o, gmax = p.gmax_o, tau_s = p.tau_s_o)
 
 class input_neuron(object):
-	def __init__(self, input_dim, vth = 1):
-		self.vth = vth
+	def __init__(self, input_dim = 3):
+		self.vth = p.vth_i
 		self.dim = input_dim
+		self.a = p.a_i
+		self.b = p.b_i
 		self.spike =np.zeros(input_dim)
 		self.v = np.zeros(input_dim)
+		self.T = p.T
+		self.dt = p.dt
+		self.t_rest = p.t_rest_i
+		self.refract = self.t_rest * np.ones(self.dim)
 
-	def forward(self, x, T, dt):
-		a = 0.2
-		b = 0.025
-		time = np.arange(0, T + dt, dt) # total simulation time
+	def forward(self, x):
+		time = np.arange(0, self.T + self.dt, self.dt) # total simulation time
 		self.vm = np.zeros((self.dim, len(time))) # membrane potential
 		for n in range(self.dim):
-			t_rest = self.t_rest
 			for i, t in enumerate(time):
-				if t > t_rest:
-					self.vm[n, i] = self.vm[n, i - 1] + (a * x[n] + b) * dt
-					if self.vm[n, i] > self.vth:
-						t_rest = t + self.tau_ref
+				if self.refract[n] > 0 :
+					self.refract[n] -=1
+				else:
+					self.vm[n, i] = self.vm[n, i - 1] + (self.a * x[n] + self.b) * self.dt
+				if self.vm[n, i] > self.vth:
+					self.refract[n] = self.t_rest
 		return self.vm, self.vm > self.vth 
 
 	def forward_t(self, x, t):
+		# pdb.set_trace()
 		dvdt = np.zeros(self.dim)
 		for i in range(self.dim):
 			self.spike[i] = 0
-			dvdt[i] = 0.2*x[i]+0.025
-			self.v[i] += dvdt[i]
-			if self.v > self.vth:
-				self.v = 0
+			dvdt[i] = self.a*x[i]+self.b
+			self.v[i] += dvdt[i] * self.dt
+			if self.v[i] > self.vth:
+				self.v[i] = 0
 				self.spike[i] = 1
-
-class Output_Neuron(LIF_Neuron):
-	def __init__(self, dim, vth = 30, tau_ref = 3, tau_m = 10, t_rest = 5, gmax = 30, tau_s = 5, alpha = 5, beta = 0.05, gamma = 0):
-		LIF_Neuron.__init__(self, dim, vth, tau_ref, tau_m, t_rest, gmax, tau_s)
-		self.alpha = alpha
-		self.beta = beta
-		self.gamma = gamma
-
-	def decode(self, x, T, dt, w):
-		time = np.arange(0, T + dt, dt)
-		_, out = self.forward(x, T, dt, w)
-		y_decode = np.zeros((self.dim, len(time)))
-		for n in range(self.dim):
-			for tf in np.where(out[n] > 0)[0]:
-				mask = np.zeros(len(time))
-				mask[tf :] = 1
-				y_decode[n] += self.alpha * (T - time[tf]) / T * np.exp(self.beta *(time[tf] - time)) * mask
-			y_decode[n] -= self.gamma
-		return out, y_decode
+		return self.spike
 
 
 if __name__ == '__main__':
 	input_dim = 3
 	hidden_dim = 2
-	input = np.random.rand(input_dim)
-	input = np.array([0.2, 0.1, 0.3])
-	input_neuron = Input_Neuron(input_dim, tau_ref = 1)
-	out, out1 = input_neuron.forward(input, 50, 0.125)
-	w = np.random.rand(3,2)
-	hidden_neuron = LIF_Neuron(hidden_dim)
-	out2, out3 = hidden_neuron.forward(out1, 50, 0.125, w)
-	output_neuron = Output_Neuron(2, gamma = 2)
-	w2 = np.random.rand(2,2)
-	out4, out5 = output_neuron.forward(out3, 50, 0.125, w2)
-	_, out6 = output_neuron.decode(out3, 50, 0.125, w2)
-	# out = synapse(out1, w, 0.125, 50)
-	plt.subplot(731)
-	plt.plot(out1[0])
-	plt.subplot(732)
-	plt.plot(out1[1])
-	plt.subplot(733)
-	plt.plot(out1[2])
-	plt.subplot(734)
-	plt.plot(out[0])
-	plt.subplot(735)
-	plt.plot(out[1])
-	plt.subplot(736)
-	plt.plot(out[2])
-	plt.subplot(737)
-	plt.plot(out2[0])
-	plt.subplot(738)
-	plt.plot(out2[1])
-	plt.subplot(7, 3, 10)
-	plt.plot(out3[0])
-	plt.subplot(7, 3, 11)
-	plt.plot(out3[1])
-	plt.subplot(7, 3, 13)
-	plt.plot(out4[0])
-	plt.subplot(7, 3, 14)
-	plt.plot(out4[1])
-	plt.subplot(7, 3, 16)
-	plt.plot(out5[0])
-	plt.subplot(7, 3, 17)
-	plt.plot(out5[1])
-	plt.subplot(7, 3, 19)
-	plt.plot(out6[0])
-	plt.subplot(7, 3, 20)
-	plt.plot(out6[1])
+	input = np.array([0.1, 0.1, 0.1])
+
+	input_neuron = input_neuron(input_dim)
+	hidden_neuron = hidden_neuron(hidden_dim)
+	output_neuron = output_neuron(2)
+	w1 = 2 * np.random.rand(3,2) - 1
+	w2 = 2 * np.random.rand(2,2) - 1
+	#not sure if membrane potential can be negative
+
+	out1T = np.zeros((input_dim, int(p.T/p.dt)))
+	out2T = np.zeros((hidden_dim, int(p.T/p.dt)))
+	out3T = np.zeros((2, int(p.T/p.dt)))
+
+	_, out1T = input_neuron.forward(input)
+	out2T, _ = hidden_neuron.forward(out1T, w1)
+	_, out3T = output_neuron.decode(out2T, w2)
+
+	# for i in range(int(p.T/p.dt)):
+	# 	out1 = input_neuron.forward_t(input, i * p.dt)
+	# 	out1T[:,i] = out1
+	# 	out2 = hidden_neuron.forward_t(i*p.dt, out1, w1)
+	# 	out2T[:,i] = out2
+	# 	out3 = output_neuron.forward_t(i*p.dt, out2, w2)
+	# 	out3T[:,i] = out3
+
+
+	plt.subplot(331)
+	plt.plot(out1T[0])
+	plt.subplot(332)
+	plt.plot(out1T[1])
+	plt.subplot(333)
+	plt.plot(out1T[2])
+	plt.subplot(334)
+	plt.plot(out2T[0])
+	plt.subplot(335)
+	plt.plot(out2T[1])
+	plt.subplot(337)
+	plt.plot(out3T[0])
+	plt.subplot(338)
+	plt.plot(out3T[1])
 	plt.show()
---
+++ parameters.py
@@ -0,0 +1,38 @@
+eta = 0.09 #learning rate
+#parameters for rewards
+taupre = taupost = 10
+Apre = 0.1
+Apost = 1.05 * Apre
+xb = 0.62#see paper two-trace model P675
+yb = 0.66
+yc = 0.28
+A_plus = 0.86/60
+A_minus = 0.25/60
+tau_plus = 19
+tau_minus = 34
+
+#input layer
+vth_i = 1
+a_i = 0.2
+b_i = 0.025
+t_rest_i = 1
+
+#hidden layer
+vth_h = 30
+tau_ref_h = 4
+tau_m_h = 10
+t_rest_h = 5
+tau_s_h = 5
+gmax_h = 30
+
+#output layer
+vth_o = 30
+tau_ref_o = 4
+tau_m_o = 10
+t_rest_o = 5
+tau_s_o = 5
+gmax_o = 30
+
+#simulation
+T = 50
+dt = 0.1
